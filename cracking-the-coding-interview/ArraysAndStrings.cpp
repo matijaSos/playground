1f74deb353d5c170c09506786941e2d7feb0b019
MatijaSos <matija.sosic@gmail.com>
2013-02-20 17:33:48 +0100

    Solved more problems

--- cracking-the-coding-interview/ArraysAndStrings.cpp
+++ cracking-the-coding-interview/ArraysAndStrings.cpp
@@ -9,7 +9,9 @@
 
 using namespace std;
 
-/** 1.1
+// ------------------------------------ 1.1 -------------------------------------- //
+
+/** 
  *  Implement an algorithm to determine if string has all unique characters.
  *  What if we can't use map?
  */
@@ -27,7 +29,8 @@ bool hasUniqueChars(string s)
     return true;
 }
 
-// If we can't use map, we can just use ascii codes as keys
+// If we can't use map, we can just use ASCII codes as keys
+// Works only for lowercase letters
 int abcSize = 26;
 bool hasUniqueChars_noMap(string s)
 {
@@ -45,10 +48,57 @@ bool hasUniqueChars_noMap(string s)
    return true;
 }
 
+// ------------------------------------ 1.2 -------------------------------------- //
+
+/**
+ *  Implement a function which reverses a null-terminated string
+ *  In-place solution.
+ */
+char* reverseString(char* s, int size)
+{
+    int lo = 0;
+    int hi = size - 1;
+
+    while (lo < hi)
+    {
+        char tmp = s[lo];
+        s[lo] = s[hi];
+        s[hi] = tmp;
+
+        lo++; hi--;
+    }
+    return s;
+}
+
+// ------------------------------------ 1.3 -------------------------------------- //
+
+/**
+ *  Given two strings, write a method to decide if one is a permutation of another.
+ */
+bool isPermutation(string a, string b)
+{
+    if (a.size() != b.size()) return false;
+
+    // Count char appearances in a
+    map<char, int> aChars;
+    for (int i = 0; i < a.size(); i++)
+        aChars[a[i]]++;
+
+    // Check b with a
+    for (int i = 0; i < b.size(); i++)
+    {
+        aChars[b[i]]--;
+        if (aChars[b[i]] < 0) return false;
+    }
+    return true;
+}
+
 /** Main, for testing
  */
 int main(int argc, char const *argv[])
 {
+    cout << "// ------------------------------- 1.1 ------------------------------- //" << endl;
+
     cout << "With map: " << endl;
     cout << "matija " << hasUniqueChars("matija") << endl;
     cout << "ivan " << hasUniqueChars("ivan") << endl;
@@ -56,6 +106,16 @@ int main(int argc, char const *argv[])
     cout << "Without map: " << endl;
     cout << "matija " << hasUniqueChars_noMap("matija") << endl;
     cout << "ivan " << hasUniqueChars_noMap("ivan") << endl;
+
+    cout << "// ------------------------------- 1.2 ------------------------------- //" << endl;
+
+    char s1_2[] = "coding";
+    cout << reverseString(s1_2, 6) << endl;
+
+    cout << "// ------------------------------- 1.3 ------------------------------- //" << endl;
+
+    cout << "coding, interview -> " << isPermutation("coding", "interview") << endl;
+    cout << "coding, gincod -> " << isPermutation("coding", "gincod") << endl;
 
     return 0;
 }
