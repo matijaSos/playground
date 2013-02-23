9535bb5a158bc55218f6e21f9ec62ded31281b20
MatijaSos <matija.sosic@gmail.com>
2013-02-23 20:31:01 +0100

    Solved 1.6

--- cracking-the-coding-interview/ArraysAndStrings.cpp
+++ cracking-the-coding-interview/ArraysAndStrings.cpp
@@ -4,8 +4,11 @@
 
 #include <iostream>
 #include <map>
+#include <sstream>
 #include <string>
 #include <cstring>
+#include <cstdio>
+#include <cstdlib>
 
 using namespace std;
 
@@ -112,7 +115,7 @@ bool isPermutation(string a, string b)
     if (a.size() != b.size()) return false;
 
     // Count char appearances in a
-    map<char, int> aChars;
+    map<char, int> aChars;                  // Maybe better to use array instead of map, its faster!
     for (int i = 0; i < a.size(); i++)
         aChars[a[i]]++;
 
@@ -125,29 +128,130 @@ bool isPermutation(string a, string b)
     return true;
 }
 
+/** Solution with sorting
+ *  Sort each array - see if they are the same
+ */
+
+// ------------------------------------ 1.4 -------------------------------------- //
+/**
+ *  Write a method to replace all spaces in string with "%20". There is enough space at the end of the string.
+ *  Do it in place.
+ *
+ *  Good questions:
+ *      1. What about multiple spaces? (e.g. "first     second        third")
+ *
+ */
+
+/** Solution - Move each word for 2 places for each space ahead of it
+ *  
+ *  1. There are no multiple spaces
+ */
+char* replaceSpaces(char* s, int size)
+{
+    // Count spaces (only those between words)
+    int spaceNum = 0;
+    for(int i = 0; i < size - 1; i++)
+        if (s[i] == ' ' && s[i + 1] != ' ')
+            spaceNum++;
+    
+    // Move each word, starting from last one
+    int wordLength = 0;
+    for (int i = size - 1; i >= 0; i--)
+    {
+        if (s[i] == ' ' && wordLength > 0) 
+        {
+            // Move word
+            int curr = i + wordLength; // Last char in word
+            int offset = 2 * spaceNum;
+            while (wordLength > 0)
+            {
+                s[curr + offset] = s[curr];
+
+                curr--;
+                wordLength--;
+            }
+            // Add space
+            strncpy(s + curr + offset - 2, "%20", 3);
+
+            spaceNum--;
+        }
+        if (s[i] != ' ')
+            wordLength++;
+    }
+    return s;
+}
+
+// ------------------------------------ 1.5 -------------------------------------- //
+/**
+ *  Compress string -> "aaabb" becomes "a3b2"
+ *  If compressed string is bigger than original, return original
+ */
+string compress(string s)
+{
+    string comp = "";
+    for (int i = 0 ; i < s.size(); i++)
+    {
+        int rep = 1;
+        while(i + 1 < s.size() && s[i + 1] == s[i])
+        {
+            rep++; i++;
+        }
+        stringstream ss;
+        ss << rep;
+        
+        // (+) causes new buffer for each element, so it's better to do (+=) twice
+        comp += s[i]; 
+        comp += ss.str();           
+    }
+
+    return (comp.size() < s.size()) ? comp : s;
+}
+
+// ------------------------------------ 1.6 -------------------------------------- //
+/**
+ *  Rotate the square matrix by 90 degrees left - in place.
+ *
+ *  Good questions:
+ *      1. Don't have any right now. Something regarding constraints?
+ */
+
+int** rotateMatrix(int** m, int size)
+{
+    for (int layer = 0; layer < (size / 2); layer++)
+    {
+        int first = layer;
+        int last = size - 1 - layer;
+        for (int i = first; i < last; i++)
+        {
+            int offset = i;
+
+            int top = m[first][i];
+
+            // Right -> top
+            m[first][i] = m[first + offset][last];
+
+            // Bottom -> right
+            m[first + offset][last] = m[last][last - offset];
+
+            // Left -> bottom
+            m[last][last - offset] = m[last - offset][first];
+            
+            // Top -> left
+            m[last - offset][first] = top;
+        }
+    }
+    return m;
+}
+
 /** Main, for testing
  */
 int main(int argc, char const *argv[])
 {
-    cout << "// ------------------------------- 1.1 ------------------------------- //" << endl;
 
-    cout << "With map: " << endl;
-    cout << "matija " << hasUniqueChars("matija") << endl;
-    cout << "ivan " << hasUniqueChars("ivan") << endl;
-    
-    cout << "Without map: " << endl;
-    cout << "matija " << hasUniqueChars_noMap("matija") << endl;
-    cout << "ivan " << hasUniqueChars_noMap("ivan") << endl;
+    string str = "ab";
+    cout << compress(str) << endl;
 
-    cout << "// ------------------------------- 1.2 ------------------------------- //" << endl;
 
-    char s1_2[] = "coding";
-    cout << reverseString(s1_2, 6) << endl;
-
-    cout << "// ------------------------------- 1.3 ------------------------------- //" << endl;
-
-    cout << "coding, interview -> " << isPermutation("coding", "interview") << endl;
-    cout << "coding, gincod -> " << isPermutation("coding", "gincod") << endl;
 
     return 0;
 }
