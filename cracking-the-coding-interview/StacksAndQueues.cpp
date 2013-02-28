d4310188766c6f11de82049f62aede7a05d5d80d
MatijaSos <matija.sosic@gmail.com>
2013-02-28 17:14:18 +0100

    Solved (conceptually) 3.1

---
+++ cracking-the-coding-interview/StacksAndQueues.cpp
@@ -0,0 +1,20 @@
+/**
+ *  Stacks and queues
+ */
+
+#include <iostream>
+
+using namespace std;
+
+// ------------------------------------ 3.1 -------------------------------------- //
+/**
+ *  Describe how you could use a single array to implement three stacks.
+ */
+
+/** Solution 1 - Fixed size to each stack (e.g. thirds)
+ */
+
+/** Solution 2 - Stacks are flexible in size
+ *  
+ *  Idea: When there is not enough space, expand stack by moving elements of others.
+ */
